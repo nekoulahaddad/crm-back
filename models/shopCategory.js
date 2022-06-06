8cdd019bcf69a1c37d5b4e5da94d6693830a7526
nekoulahaddad <nekoulahaddad@gmail.com>
2022-06-06 12:26:44 +0400

    add-visible-filled

--- models/shopCategory.js
+++ models/shopCategory.js
@@ -3,12 +3,13 @@ const { Schema, Types, model } = mongoose;
 
 const shopCategorySchema = new Schema(
   {
-    category: [{ type: Types.ObjectId, ref: 'category' }],
-    shop: { type: Types.ObjectId, ref: 'shop' }
+    category: [{ type: Types.ObjectId, ref: "category" }],
+    shop: { type: Types.ObjectId, ref: "shop" },
+    visible: { type: Boolean, required: true, default: true },
   },
   {
     timestamps: true,
   }
 );
 
-export const ShopCategory = model('ShopCategory', shopCategorySchema);
+export const ShopCategory = model("ShopCategory", shopCategorySchema);
