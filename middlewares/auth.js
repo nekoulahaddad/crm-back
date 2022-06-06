692a4a70449e68ef107d344bf6f685ad96ea8414
nekoulahaddad <nekoulahaddad@gmail.com>
2022-06-06 15:55:54 +0400

    checkPartner-route+middleWare

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ import { User } from "../models/user.js";
 
 export const auth = async (req, res, next) => {
   try {
-    const token = req.headers["authorization"];
+    const token = req.headers["authorization"].split(" ")[1];
     if (!token) {
       return res.send({
         status: 403,
@@ -14,14 +14,34 @@ export const auth = async (req, res, next) => {
     }
     const user = await jwt.verify(token, JWT_SECRET);
     req.token = token;
-    const userObject = user ? await User.findById(user._id) : null;
+    const userObject = user
+      ? await User.findById(user._id).populate("role")
+      : null;
     req.user = userObject;
-    req.shop_id = userObject.shop_id ? userObject.shop_id : null;
     return next();
   } catch (error) {
     res.send({
       status: 401,
-      message: error.message, //"Токен истёк",
+      message: "Токен истёк",
+    });
+  }
+};
+
+export const checkPartner = async (req, res, next) => {
+  try {
+    const user = req.user;
+    if (user.role.value !== "partner") {
+      return res.send({
+        status: 403,
+        message: "не доступен",
+      });
+    }
+    req.shop_id = user.shop_id ? user.shop_id : null;
+    return next();
+  } catch (error) {
+    res.send({
+      status: 401,
+      message: error.message,
     });
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,12 +5,13 @@ import {
   testAuth,
   refreshToken,
 } from "../controllers/authController.js";
-import { auth } from "../middlewares/auth.js";
+import { auth, checkPartner } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.get("/logout/:id", logoutUser);
 router.get("/refreshToken", refreshToken);
 router.get("/testAuth", auth, testAuth);
+router.get("/checkPartner", auth, checkPartner, testAuth);
 
 export default router;
