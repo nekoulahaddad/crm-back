38c300c5a62a2864f1b40bc9a5ab5cdc612de188
nekoulahaddad <nekoulahaddad@gmail.com>
2022-06-07 10:57:39 +0400

    change-shopCategoryModel

--- models/shopCategory.js
+++ models/shopCategory.js
@@ -3,8 +3,8 @@ const { Schema, Types, model } = mongoose;
 
 const shopCategorySchema = new Schema(
   {
-    category: [{ type: Types.ObjectId, ref: "category" }],
-    shop: { type: Types.ObjectId, ref: "shop" },
+    category: { type: Types.ObjectId, ref: "Category" },
+    shop: { type: Types.ObjectId, ref: "Shop" },
     visible: { type: Boolean, required: true, default: true },
   },
   {
