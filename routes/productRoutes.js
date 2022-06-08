b52673372d7ccc508d9d613830d3776c51a0e4d9
Halid <denievhalid88@mail.ru>
2022-06-08 17:58:19 +0300

    Добавил метод для добавления/удаления продукта из рекомендаций

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { changeSeo, clearSeo, insertProduct } from "../controllers/productController.js";
+import {
+  changeSeo,
+  clearSeo,
+  insertProduct,
+  setIsRecommended,
+} from "../controllers/productController.js";
+import { auth, checkPartner } from "../middlewares/auth";
 const router = express.Router();
 
 router.get("/insertProduct", insertProduct);
 router.patch("/seo/change/:id", changeSeo);
 router.patch("/seo/clear/:id", clearSeo);
+router.patch("/setIsRecommended", auth, checkPartner, setIsRecommended);
 
 export default router;
