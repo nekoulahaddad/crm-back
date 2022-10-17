bfdd6c396245da9e35618c4aa62ab35f49dadfe4
nekoulahaddad <nekoulahaddad@gmail.com>
2022-10-17 21:33:56 +0400

    i wanna change this msg

--- controllers/statusOrderController.js
+++ controllers/statusOrderController.js
@@ -8,11 +8,6 @@
     });
   } catch (error) {
     res.status(500).send({
-      status: "error",
-      error: {
-        name: error.name,
-        message: error.message
-      }
-    })
+
   }
 };
