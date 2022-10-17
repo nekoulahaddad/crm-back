03b8e6b4027a1d47149870489a1b7f7a2fa693b7
nekoulahaddad <nekoulahaddad@gmail.com>
2022-10-17 21:34:22 +0400

    bye bye 7abibbi

--- controllers/mainCategoryController.js
+++ controllers/mainCategoryController.js
@@ -1,15 +1,6 @@
 
-      data: newCategory,
-    });
-  } catch (error) {
-    res.status(500).send({
-      status: "error",
-      error: {
 
 
-
-
-        
         name: error.name,
         message: error.message
       }
